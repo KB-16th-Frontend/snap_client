0c4314af28a4db5e334ffaf1df97e8d8bcb75da3
wherewhale <syh980130@gmail.com>
2025-04-06 17:20:33 +0900

    feat(loading): add lottie loading layout #7

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
   </CommonBody>
 
   <CommonFooter />
+
+  <LottieLoading v-if="isLoading" />
 </template>
 
 <script setup>
@@ -16,4 +18,15 @@ import { RouterView } from 'vue-router';
 import CommonHeader from './components/layouts/CommonHeader.vue';
 import CommonFooter from './components/layouts/CommonFooter.vue';
 import CommonBody from './components/layouts/CommonBody.vue';
+import LottieLoading from '@/components/common/Loading/LottieLoading.vue';
+import { onMounted, ref } from 'vue';
+
+const isLoading = ref(false);
+
+onMounted(() => {
+  isLoading.value = true;
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000); // 2초 후 로딩 종료
+});
 </script>
---
+++ src/components/common/Loading/LottieLoading.vue
@@ -0,0 +1,13 @@
+<template>
+  <Teleport to="#loading">
+    <div class="loading-overlay" role="alert" aria-busy="true" aria-live="assertive">
+      <Vue3Lottie :width="200" :height="200" :animation-data="loadingJson" />
+    </div>
+  </Teleport>
+</template>
+
+<script setup>
+import './LottieLoading.css';
+import { Vue3Lottie } from 'vue3-lottie';
+import loadingJson from '@/assets/lottie/loading.json';
+</script>
