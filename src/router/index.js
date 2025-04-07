fe86b3b3ad81b78e60a50544a26bb33a8c3ac2a5
wha1e <syh980130@gmail.com>
2025-04-07 11:12:48 +0900

    feat: add routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage,
-    },
-  ],
-});
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'root',
+            redirect: '/home',
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: HomePage,
+        },
+        {
+            path: '/payments',
+            name: 'payments',
+            component: () => import('../pages/PaymentsPage.vue'),
+        },
+        {
+            path: '/charts',
+            name: 'charts',
+            component: () => import('../pages/ChartsPage.vue'),
+        },
+        {
+            path: '/mypage',
+            name: 'mypage',
+            component: () => import('../pages/MyPage.vue'),
+        },
+    ],
+})
 
-export default router;
+export default router
