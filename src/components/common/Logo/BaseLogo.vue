49b69e5ae7912b45ae297c2e60c40983eb73197c
wha1e <syh980130@gmail.com>
2025-04-07 15:15:27 +0900

    feat: add logo button

---
+++ src/components/common/Logo/BaseLogo.vue
@@ -0,0 +1,25 @@
+<style scoped>
+.logo-container {
+    display: block;
+    width: 85px;
+}
+
+.logo {
+    display: block;
+    width: 100%;
+    height: auto;
+}
+.logo-image {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+}
+</style>
+
+<template>
+    <router-link to="home" class="logo-container navbar-brand">
+        <figure class="logo">
+            <img src="@/assets/images/logo.png" alt="Snap Logo Image" class="logo-image" />
+        </figure>
+    </router-link>
+</template>
