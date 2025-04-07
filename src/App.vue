49aa700957b5ebd45b22ba4fb4b21568a7322f2a
wha1e <syh980130@gmail.com>
2025-04-07 10:55:11 +0900

    feat: add base layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <style scoped></style>
 
 <template>
-  <!-- 헤더와 푸터는 고정 -->
-  <CommonHeader />
-
-  <CommonBody>
+    <!-- 헤더와 푸터는 고정 -->
     <RouterView />
-  </CommonBody>
-
-  <CommonFooter />
-
-  <LottieLoading v-if="isLoading" />
+    <LottieLoading v-if="isLoading" />
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
-import CommonHeader from './components/layouts/CommonHeader.vue';
-import CommonFooter from './components/layouts/CommonFooter.vue';
-import CommonBody from './components/layouts/CommonBody.vue';
-import LottieLoading from '@/components/common/Loading/LottieLoading.vue';
-import { onMounted, ref } from 'vue';
+import { RouterView } from 'vue-router'
+import LottieLoading from '@/components/common/Loading/LottieLoading.vue'
+import { onMounted, ref } from 'vue'
 
-const isLoading = ref(false);
+const isLoading = ref(false)
 
 onMounted(() => {
-  isLoading.value = true;
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 2000); // 2초 후 로딩 종료
-});
+    isLoading.value = true
+    setTimeout(() => {
+        isLoading.value = false
+    }, 2000) // 2초 후 로딩 종료
+})
 </script>
