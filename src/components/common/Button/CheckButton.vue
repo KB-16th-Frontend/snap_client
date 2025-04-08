55e1477d1ee196c748bb98c4690e267611a447c0
KyungHwan Jeon <crasheso@gmail.com>
2025-04-08 10:53:26 +0900

    refactor : button color changed

--- src/components/common/Button/CheckButton.vue
+++ src/components/common/Button/CheckButton.vue
@@ -1,16 +1,15 @@
 <template>
-    <button class="check-btn" :class="{selected:isSelected}" @click="onClick" >
+    <button class="check-btn" :class="{ selected: isSelected }" @click="onClick">
         <span class="me-2" v-if="isSelected"><i class="fa-solid fa-check" color="white"></i></span>
         <slot></slot>
     </button>
-   </template>
+</template>
 
 <script setup>
 defineProps({
-    onClick : Function,
+    onClick: Function,
     isSelected: Boolean,
 })
-
 </script>
 
 <style scoped>
@@ -20,16 +19,20 @@ defineProps({
     font-size: 1rem;
     font-weight: bold;
     border-radius: 1.2rem;
-    border: 1px solid #1e3050;
-    background-color: white;
-    color: #1e3050;
+    /* border: 1px solid #1e3050; */
+    border: 1px solid var(--vt-c-primary);
+    border: 1px solid;
+    /* background-color: white; */
+    background-color: var(--vt-c-white);
+    color: var(--vt-c-primary);
     cursor: pointer;
     transition: 0.2s;
 }
 
 .check-btn.selected {
-    background-color: #1e3050;
-    color: white;
+    background-color: var(--vt-c-primary);
+    /* color: white; */
+    color: var(--vt-c-white);
     font-weight: bold;
 }
 </style>
