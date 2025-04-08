b88cf0761aa2bf483a52915027e9d3e85cedb900
cherryiJuice <chldlwn711@gmail.com>
2025-04-08 13:15:29 +0900

    refactor: 세부 컴포넌트화 및 pinia 적용
    가치/낭비 소비 차트, 카테고리 차트 컴포넌트화 및 pinia 적용

---
+++ src/components/chart/SpendTypeChart.vue
@@ -0,0 +1,31 @@
+<template>
+    <BaseTypography size="lg" weight="bold" class="w-100 text-start mb-2 pb-1"
+        >가치/낭비 소비</BaseTypography
+    >
+    <BaseCard>
+        <BasePieChart :labels="['가치 소비', '낭비 소비']" :data="[goodSpending, badSpending]" />
+        <BaseTypography size="md" weight="bold" class="d-flex justify-content-between w-100 mb-2">
+            <span>가치 소비</span>
+            <span>{{ goodSpending.toLocaleString() }}원</span>
+        </BaseTypography>
+        <BaseTypography size="md" weight="bold" class="d-flex justify-content-between w-100">
+            <span>낭비 소비</span>
+            <span>{{ badSpending.toLocaleString() }}원</span>
+        </BaseTypography>
+    </BaseCard>
+</template>
+
+<script setup>
+import BasePieChart from '@/components/chart/BasePieChart.vue'
+import BaseCard from '@/components/common/Card/BaseCard.vue'
+import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
+
+const props = defineProps({
+    goodSpending: {
+        type: Number,
+    },
+    badSpending: {
+        type: Number,
+    },
+})
+</script>
--- src/pages/PaymentsReportPage.vue
+++ src/pages/PaymentsReportPage.vue
@@ -3,47 +3,26 @@
 <template>
     <BaseLayout>
         <section class="w-100 pb-3">
-            <!-- 컴포넌트 레이아웃 변경 후 bg-light-blue 배경 색 추가-->
+            <!-- 베이스 레이아웃 변경 후 bg-light-blue 배경 색 추가-->
             <BaseTypography size="lg" weight="bold" class="w-100 text-start mb-2"
-                >4월 지출</BaseTypography
+                >{{ month }}월 지출</BaseTypography
             >
             <BaseTypography color="dark" size="xl" weight="bold" class="text-start mb-1"
-                >123,123원</BaseTypography
+                >{{ totalSpending.toLocaleString() }}원</BaseTypography
             >
         </section>
-
-        <BaseTypography size="lg" weight="bold" class="w-100 text-start mb-2 pb-1"
-            >가치/낭비 소비</BaseTypography
-        >
-        <BaseCard>
-            <PieChart :labels="['가치 소비', '낭비 소비']" :data="[70, 30]" />
-            <BaseTypography
-                size="md"
-                weight="bold"
-                class="d-flex justify-content-between w-100 mb-2"
-            >
-                <span>가치 소비</span>
-                <span>123,456원</span>
-            </BaseTypography>
-            <BaseTypography size="md" weight="bold" class="d-flex justify-content-between w-100">
-                <span>낭비 소비</span>
-                <span>123,456원</span>
-            </BaseTypography>
-        </BaseCard>
-        <BaseTypography size="lg" weight="bold" class="w-100 text-start mt-4 mb-3"
-            >카테고리별 지출</BaseTypography
-        >
-        <BaseCard>
-            <PieChart :labels="['식비', '카페', '교육', '미용']" :data="[70, 30, 20, 10]" />
-            <PaymentItem></PaymentItem>
-        </BaseCard>
+        <SpendTypeChart :goodSpending="goodSpending" :badSpending="badSpending"></SpendTypeChart>
+        <CategoryChart :categorySpendings="categorySpendings"></CategoryChart>
     </BaseLayout>
 </template>
 
 <script setup>
-import PieChart from '@/components/chart/basePieChart.vue'
-import BaseCard from '@/components/common/Card/BaseCard.vue'
+import { usePaymentsReportStore } from '@/stores/counter'
 import BaseLayout from '@/components/layouts/BaseLayout.vue'
 import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
-import PaymentItem from '@/components/common/PaymentItem/PaymentItem.vue'
+import SpendTypeChart from '@/components/chart/SpendTypeChart.vue'
+import CategoryChart from '@/components/chart/CategoryChart.vue'
+
+const { month, totalSpending, goodSpending, badSpending, categorySpendings } =
+    usePaymentsReportStore()
 </script>
