5e7bbc3b2188699fa884991f41f90e0e874a9f5d
cherryiJuice <chldlwn711@gmail.com>
2025-04-08 10:44:56 +0900

    refactor: 컴포넌트 이름 변경

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
         {
             path: '/paymentsReport',
             name: 'report',
-            component: () => import('../pages/PaymentsReportPage.vue'),
+            component: () => import('../pages/StatisticsPage.vue'),
         },
     ],
 })
