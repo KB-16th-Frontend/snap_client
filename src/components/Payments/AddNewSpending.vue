3d0644e7bf9dce1882ee24bfc6ede207798f9a79
hyungEee <jihyung772@gmail.com>
2025-04-08 15:42:13 +0900

    feat:PaymentsPage 및 관련 컴포넌트 추가

---
+++ src/components/Payments/AddNewSpending.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="w-100">
+        <BaseTypography tag="h2" color="primary" size="lg" weight="bold" class="text-start w-100">
+            새로운 지출이 생겼나요?
+        </BaseTypography>
+
+        <BaseCard title="수입 및 지출 추가" class="mt-3">
+            <BaseTypography color="black" size="medium" weight="bold" class="mt-3">
+                새로운 수입 및 지출이 생겼나요?</BaseTypography
+            >
+            <BaseTypography color="black" size="medium" weight="bold" class="mt-2"
+                >스냅과 함께 하루를 기록해보세요!</BaseTypography
+            >
+            <router-link class="btn btn-primary mt-3" to="/">
+                <BaseTypography color="white" size="medium" weight="bold"
+                    >수입 및 지출 추가하기</BaseTypography
+                >
+            </router-link>
+        </BaseCard>
+    </section>
+</template>
+<script setup>
+import BaseCard from '../common/Card/BaseCard.vue'
+import BaseTypography from '../common/Typography/BaseTypography.vue'
+</script>
---
+++ src/components/Payments/SpendingReport.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="w-100">
+        <BaseTypography tag="h2" color="primary" size="lg" weight="bold" class="text-start w-100">
+            지출 분석 리포트
+        </BaseTypography>
+        <BaseCard title="지출 분석 리포트" class="mt-3" toName="charts">
+            <BaseTypography size="lg" weight="bold" class="mt-3"
+                >가장 많은 돈을 쓴 카테고리
+            </BaseTypography>
+            <BaseTypography size="xl" weight="black" color="dark" class="mt-3"
+                >#{{ props.category }}</BaseTypography
+            >
+        </BaseCard>
+    </section>
+</template>
+<script setup>
+import BaseCard from '../common/Card/BaseCard.vue'
+import BaseTypography from '../common/Typography/BaseTypography.vue'
+import { defineProps } from 'vue'
+const props = defineProps({
+    category: String,
+})
+</script>
