7399897dd6a995c165ebe4dbeb8ce2b99d50b47c
cherryiJuice <chldlwn711@gmail.com>
2025-04-08 13:15:29 +0900

    refactor: 세부 컴포넌트화 및 pinia 적용
    가치/낭비 소비 차트, 카테고리 차트 컴포넌트화 및 pinia 적용

---
+++ src/components/chart/CategoryChart.vue
@@ -0,0 +1,22 @@
+<template>
+    <BaseTypography size="lg" weight="bold" class="w-100 text-start mt-4 mb-3"
+        >카테고리별 지출</BaseTypography
+    >
+    <BaseCard>
+        <BasePieChart :labels="lables" :data="data" />
+    </BaseCard>
+</template>
+
+<script setup>
+import BaseCard from '@/components/common/Card/BaseCard.vue'
+import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
+import BasePieChart from '@/components/chart/BasePieChart.vue'
+
+const props = defineProps({
+    categorySpendings: {
+        type: Array,
+    },
+})
+const lables = props.categorySpendings.map((item) => item.label)
+const data = props.categorySpendings.map((item) => Number(item.data))
+</script>
