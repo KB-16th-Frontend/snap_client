8f4593418b19f8ab5ba176072c4fac1d294b2b53
KyungHwan Jeon <crasheso@gmail.com>
2025-04-08 09:21:36 +0900

    feat : modal practice

---
+++ src/components/common/Button/NextButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button
+        class="next-btn"
+        :class="{ active: isActive }"
+        :disabled="isActive"
+        @click="handleClick"
+    >
+        다음
+    </button>
+</template>
+<script setup>
+defineProps({
+    isActive: Boolean,
+})
+</script>
+<style>
+.next-btn{
+
+}
+.next-btn.active{
+    
+}
+</style>
---
+++ src/components/common/Modal/ModalPractice.vue
@@ -0,0 +1,44 @@
+<template>
+    <button @click="openModal">Modal</button>
+    <div class="modal-wrap" v-if="isOpen">
+        <div class="modal-container">
+            tmp
+            <button @click="closeModal">닫기</button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+const openModal = () => {
+    isOpen.value = true
+}
+
+const closeModal = () => {
+    isOpen.value = false
+}
+</script>
+<style>
+.modal-wrap {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+}
+
+.modal-container {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    background: white;
+    border-radius: 10px;
+    padding: 20px;
+    box-sizing: border-box;
+}
+</style>
