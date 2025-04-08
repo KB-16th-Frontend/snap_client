3ccf88ac8048b31c2a48e76bb0eb4944f7d8ec41
cherryiJuice <chldlwn711@gmail.com>
2025-04-08 10:23:07 +0900

    refactor: path 이름 변경 및 payments 자식으로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,13 @@ const router = createRouter({
             path: '/payments',
             name: 'payments',
             component: () => import('../pages/PaymentsPage.vue'),
+            children: [
+                {
+                    path: 'statistics',
+                    name: 'statistics',
+                    component: () => import('../pages/StatisticsPage.vue'),
+                },
+            ],
         },
         {
             path: '/charts',
