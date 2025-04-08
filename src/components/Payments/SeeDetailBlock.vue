3396cb6c77f8577291821dc0124fb38f745b7e3b
hyungEee <jihyung772@gmail.com>
2025-04-08 13:10:46 +0900

    feat:PaymentsPage 및 관련 컴포넌트 추가

---
+++ src/components/Payments/SeeDetailBlock.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="w-100">
+        <BaseTypography tag="h2" color="primary" size="lg" weight="bold" class="text-start w-100">
+            소비 기록 상세 보기
+        </BaseTypography>
+        <BaseCard title="소비 기록 상세" class="mt-3" toName="">
+            <div class="d-flex mt-3">
+                <BaseTypography size="lg" weight="bold" class="me-1">총 수입 : </BaseTypography>
+                <BaseTypography size="lg" weight="bold" color="red">{{
+                    props.income.toLocaleString()
+                }}</BaseTypography>
+                <BaseTypography size="lg" weight="bold">원</BaseTypography>
+            </div>
+            <div class="d-flex">
+                <BaseTypography size="lg" weight="bold" class="me-1">총 지출 : </BaseTypography>
+                <BaseTypography size="lg" weight="bold" color="blue">{{
+                    props.spending.toLocaleString()
+                }}</BaseTypography>
+                <BaseTypography size="lg" weight="bold">원</BaseTypography>
+            </div>
+        </BaseCard>
+    </section>
+</template>
+<script setup>
+import BaseCard from '@/components/common/Card/BaseCard.vue'
+import BaseTypography from '../common/Typography/BaseTypography.vue'
+import { defineProps } from 'vue'
+const props = defineProps({
+    income: {
+        type: Number,
+    },
+    spending: {
+        type: Number,
+    },
+})
+</script>
---
+++ src/components/Payments/ThisMonthSpending.vue
@@ -0,0 +1,66 @@
+<style scoped>
+.sec {
+    background-color: #e1ecff;
+    height: 15rem;
+    position: relative;
+}
+select {
+    border-radius: 4px;
+    border: 1px solid #dee2e6;
+    padding: 0.4rem;
+    padding-right: 1.5rem;
+}
+.title-box {
+    position: absolute;
+    left: 3rem;
+    top: 2.5rem;
+}
+.select-box {
+    position: absolute;
+    right: 3rem;
+    bottom: 2.5rem;
+}
+.emoji {
+    position: absolute;
+    top: 2.5rem;
+    right: 3rem;
+}
+</style>
+<template>
+    <section class="w-100 p-3 sec">
+        <div class="d-flex flex-column title-box">
+            <BaseTypography size="lg" weight="bold">{{ props.name }} 님의</BaseTypography>
+            <BaseTypography size="lg" weight="bold"
+                >{{ selectedYear }}년 {{ selectedMonth }}월</BaseTypography
+            >
+            <BaseTypography size="lg" weight="bold">소비 기록</BaseTypography>
+        </div>
+        <BaseTypography size="2xl" class="emoji">📝</BaseTypography>
+        <div class="d-flex gap-3 select-box">
+            <select v-model="selectedYear">
+                <option v-for="(item, index) in years" :key="index" :value="item">
+                    {{ item }}년
+                </option>
+            </select>
+            <select v-model="selectedMonth">
+                <option v-for="(item, index) in months" :key="index" :value="item">
+                    {{ item }}월
+                </option>
+            </select>
+        </div>
+    </section>
+</template>
+<script setup>
+import BaseTypography from '../common/Typography/BaseTypography.vue'
+import { ref, defineProps } from 'vue'
+const props = defineProps({
+    name: {
+        type: String,
+    },
+})
+const years = [2025, 2024, 2023, 2022, 2021]
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const selectedYear = ref(new Date().getFullYear())
+const selectedMonth = ref(new Date().getMonth() + 1)
+</script>
