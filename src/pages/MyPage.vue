587b8f9e6d3a945b9362cc8ddc703c987bf5eae1
cherryiJuice <chldlwn711@gmail.com>
2025-04-09 11:34:29 +0900

    feat: 이름 추가 기능

--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -6,7 +6,12 @@
             <BaseTypography size="lg" weight="bold" color="primary" class="mx-3 py-3">
                 {{ menus.title }}
             </BaseTypography>
-            <div class="bg-white pt-3 pb-3" role="button" v-for="element in menus.elements">
+            <div
+                class="bg-white pt-3 pb-3"
+                role="button"
+                v-for="element in menus.elements"
+                @click="goToAddPage"
+            >
                 <BaseTypography size="md" weight="medium" color="primary" class="mx-3">
                     {{ element.name }}</BaseTypography
                 >
@@ -16,7 +21,15 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 import BaseLayout from '@/components/layouts/BaseLayout.vue'
 import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
 import { MY_PAGE_MENUS } from '@/common/constants'
+
+const router = useRouter()
+const memberId = 1
+
+const goToAddPage = () => {
+    router.push(`/mypage/add/${memberId}`)
+}
 </script>
