1b38932c8f434e0a3b07c2ca2f02808884d7aab5
cherryiJuice <chldlwn711@gmail.com>
2025-04-10 13:48:19 +0900

    feat: 카테고리 데이터 pinia에 저장

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,19 @@
 import { RouterView } from 'vue-router'
 import LottieLoading from '@/components/common/Loading/LottieLoading.vue'
 import { onMounted, ref } from 'vue'
+import { useCategoryStore } from '@/stores/categoryStore'
+import { fetchCategories } from '@/api/category'
 
 const isLoading = ref(false)
+const categoryStore = useCategoryStore()
 
-onMounted(() => {
+onMounted(async () => {
     isLoading.value = true
     setTimeout(() => {
         isLoading.value = false
-    }, 2000) // 2초 후 로딩 종료
+    }, 2000)
+
+    const data = await fetchCategories()
+    categoryStore.setCategories(data)
 })
 </script>
