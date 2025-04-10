6168c1a3d601786af78745e64c84c83f5c981cc8
wha1e <syh980130@gmail.com>
2025-04-10 10:43:34 +0900

    fix authguard

--- src/hooks/useAuthGuard.js
+++ src/hooks/useAuthGuard.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'vue-router'
 import useUserStore from '@/stores/auth'
-import { getUserLocalStorage } from '@/utils/token'
+import { getUserLocalStorage, removeUserLocalStorage } from '@/utils/token'
 import { getUserInfo } from '@/api/auth'
 
 export async function useAuthGuard() {
@@ -17,6 +17,9 @@ export async function useAuthGuard() {
             return true // 성공적으로 유저 정보를 불러옴
         } catch (error) {
             console.error('유저 정보 불러오기 실패:', error)
+            userStore.removeUserInfo()
+            removeUserLocalStorage()
+            // 유저 정보 불러오기 실패 시, 로컬 스토리지에서 유저 정보 제거
             router.push('/')
             return false
         }
